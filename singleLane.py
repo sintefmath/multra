f14c6c378423dd961937ac352844fed5c1abab64
Franz G. Fuchs <franzgeorgfuchs@gmail.com>
2015-03-04 14:04:24 +0100

    two roads coupled with a traffic light

--- singleLane.py
+++ singleLane.py
@@ -7,35 +7,44 @@ def main(n,o, limiter, solver):
     Length = 2.
     dx = Length/n
     x = dx/2. + np.arange(n)*dx
-    rho = initializeRho(x,o)
+    rho_r1 = initializeRho(x,o)
+    rho_r2 = initializeRho(x,o)
+    rho = np.vstack([rho_r1,rho_r2])
 
     pl.ion()
     #pl.figure()
-    line, = pl.plot(x,rho[o:-o])
-    line.axes.set_ylim(-.1,1.1)
+    line1, line2, = pl.plot(x,rho[0,o:-o],'g',Length+x,rho[1,o:-o],'r')
+    line1.axes.set_ylim(-.1,1.1)
+    line2.axes.set_ylim(-.1,1.1)
     pl.draw()
 
     #pl.plot(x,rho[o:-o],label='t=0')
 
-    T = 10
+    T = 20
     Tintervals = np.linspace(0.,1.*T,num=T+1)
     for ti in range(Tintervals.shape[0]-1):
-        print Tintervals[ti:ti+2]
-        rho = ConsLaw(x, rho, Tintervals[ti:ti+2], limiter, solver, o, line)
+        #print Tintervals[ti:ti+2]
+        rho = ConsLaw(x, rho, Tintervals[ti:ti+2], limiter, solver, o, line1, line2)
         #pl.plot(x,rho[o:-o],label='t=%0.2f'%Tintervals[ti+1])
     #pl.legend()
     #pl.show()
 
-def ConsLaw(x, rho, Tinterval, limiter, solver, o, line):
+def ConsLaw(x, rho, Tinterval, limiter, solver, o, line1, line2):
 
     dx = x[1]-x[0]
     n = x.shape[0]
 
     t = cp(Tinterval[0])
 
+    count = 0
     while t<Tinterval[1]:
-        DF = Flux_drho(rho)
+        count += 1
+
+        DF = Flux_drho(rho[0,o:-o])
         eig = max(np.abs(DF))
+        DF = Flux_drho(rho[1,o:-o])
+        eig = max(eig,max(np.abs(DF)))
+
         if (o==1):
             CFL = 0.99
         if (o==2):
@@ -50,22 +59,27 @@ def ConsLaw(x, rho, Tinterval, limiter, solver, o, line):
         if o==2:
             saverho = 1.*rho
         for internalsteps in range(1,o+1):
-            rho = applyBoundaryConditions(rho,o,t)
+            rho[0,:] = BC_inflow(rho[0,:],o,t)
+            if (t<6 or (t>8 and t<10) or (t>12 and t<15) or (t>16 and t<17) ):
+                rho = BC_coupleRed(rho,o,0,1)
+            else:
+                rho = BC_coupleGreen(rho,o,0,1)
+            rho[1,:] = BC_outflow(rho[1,:],o,t)
             ## construct uL and uR
             if o==1:
-                rhoL = rho[0:-1]
-                rhoR = rho[1:]
+                rhoL = rho[:,0:-1]
+                rhoR = rho[:,1:]
             if o==2:
                 # apply slope limiter
-                a = rho[1:]-rho[0:-1]
+                a = rho[:,1:]-rho[:,0:-1]
                 if limiter=='minmod':
-                    S = minmod(a[0:-1],a[1:])
+                    S = minmod(a[:,0:-1],a[:,1:])
                 if limiter=='superbee':
-                    S = superbee(a[0:-1],a[1:])
+                    S = superbee(a[:,0:-1],a[:,1:])
                 if limiter=='mc':
-                    S = mc(2.*a[0:-1],.5*(rho[2:]-rho[:-2]),2.*a[1:])
-                rhoL = rho[1:-2] + 0.5*S[0:-1]
-                rhoR = rho[2:-1] - 0.5*S[1:]
+                    S = mc(2.*a[:,0:-1],.5*(rho[:,2:]-rho[:,:-2]),2.*a[:,1:])
+                rhoL = rho[:,1:-2] + 0.5*S[:,0:-1]
+                rhoR = rho[:,2:-1] - 0.5*S[:,1:]
             if solver=='LxF':
                 # Lax Friedrichs
                 Frho = 0.5*(Flux(rhoL) + Flux(rhoR)) - 0.5*dx/dt*(rhoR - rhoL)
@@ -77,13 +91,15 @@ def ConsLaw(x, rho, Tinterval, limiter, solver, o, line):
                 #Frho[np.nonzero((s>0.))] = Flux(rhoL[np.nonzero((s>0.))])
                 #Frho[np.nonzero((s<0.))] = Flux(rhoR[np.nonzero((s<0.))])
                 Frho[np.nonzero((rhoL>.5)&(.5>rhoR))] = Flux(.5)
-            rho[o:-o] -= dt/dx*(Frho[1:] - Frho[0:-1])
+            rho[:,o:-o] -= dt/dx*(Frho[:,1:] - Frho[:,0:-1])
         # end loop for order
         if o==2:
             rho = 0.5*(rho + saverho)
 
-        line.set_ydata(rho[o:-o])
-        pl.draw()
+        if (count%15==0):
+            line1.set_ydata(rho[0,o:-o])
+            line2.set_ydata(rho[1,o:-o])
+            pl.draw()
         #time.sleep(0.05)
 
     # end while
@@ -101,37 +117,12 @@ def Flux_drho(rho):
 def initializeRho(x,o):
     # initialize function
     rho = 0.*x
-  #  rho[np.nonzero((x>.75)&(x<1.25))]=1.
-  #  x_ = x%1.
-  #  rho = .5 + 0.*x_
-  #  dx = x[1] - x[0]
-
-  #  rho[np.nonzero((x<2.))]=1.
-
-  #  rho[np.nonzero((x_>.25)&(x_<.75))]=1.
-  #  # as this is a FV method take the integral in the cells containing a jump
-  #  inde = np.nonzero((x_-.5*dx<.25)&(x_+.5*dx>.25))
-  #  rho[inde]=(.25-(x_[inde]-.5*dx))/dx*.5 + (x_[inde]+.5*dx-.25)/dx*1.
-  #  ### case one with a sin function ###
-  #  rho[np.nonzero(x_>.5)] = np.sin(np.pi*1.5 + 6*np.pi*x_[np.nonzero(x_>.5)])/4. + .75
-  #  ### case two with a jump ###
-  #  #inde = np.nonzero((x_-.5*dx<.75)&(x_+.5*dx>.75))
-  #  #rho[inde]=(.75-(x_[inde]-.5*dx))/dx*1. + (x_[inde]+.5*dx-.75)/dx*.5
-  #  rho[x<1] = 0.5
-  #  rho[x>2] = 0.5
-  #  rho /= 1.
-
     # add ghost cells
     for i in range(1,o+1):
         rho = np.hstack([0.,rho,0.])
+    return rho
 
-    return applyBoundaryConditions(rho,o,0)
-
-def applyBoundaryConditions(rho,o,t):
-    #rho[0] = rho[1]
-    #rho[-1] = rho[-2]
-    #return rho
-
+def BC_inflow(rho,o,t):
     ### inflow ###
     #if (t>6):
     #    rho[0] = 0
@@ -140,21 +131,23 @@ def applyBoundaryConditions(rho,o,t):
     rho[0] = max(0,np.sin(6*t))
     if o==2:
         rho[1] = cp(rho[0])
+    return rho
 
-    ### outflow ###
-    if (t<6 or t>8):
-        rho[-1] = 1.
-        if o==2:
-            rho[-2] = cp(rho[-1])
-    else:
-        rho[-1] = 0.
-        if o==2:
-            rho[-2] = cp(rho[-1])
+def BC_outflow(rho,o,t):
+    for i in range(1,o+1):
+        rho[-o+i-1] = rho[-o+i-2]
+    return rho
 
-    #for i in range(1,o+1):
-    #    rho[o-i]=rho[o-i+1]
-    #    rho[-o+i-1]=rho[-o+i-2]
+def BC_coupleGreen(rho,o,roadIn, roadOut):
+    for i in range(1,o+1):
+        rho[roadIn,-o+i-1] = cp(rho[roadOut,o+i-1])
+        rho[roadOut,o-i] = cp(rho[roadIn,-o-i])
+    return rho
 
+def BC_coupleRed(rho,o,roadIn, roadOut):
+    for i in range(1,o+1):
+        rho[roadIn,-o+i-1] = 1.
+        rho[roadOut,o-i] = 0.
     return rho
 
 def minmod(a,b):
@@ -177,7 +170,7 @@ def mc(x,y,z):
 
 if __name__ == "__main__":
 
-    main(200,2,'minmod','upwind')
+    main(800,2,'minmod','upwind')
 
     #from optparse import OptionParser
     #usage = "usage: %prog [var=value]"
