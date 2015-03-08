a39f8aa9b4ef8e904d4d82c89a9ac9ccda3dd21b
Franz G. Fuchs <franzgeorgfuchs@gmail.com>
2015-03-08 21:37:55 +0100

    extension to n roads

--- singleLane.py
+++ singleLane.py
@@ -1,5 +1,4 @@
 import numpy as np
-#import pylab as pl
 from copy import copy as cp
 import time
 
@@ -8,6 +7,89 @@ from OpenGL.GLUT import *
 from OpenGL.GLU import *
 
 texture = 0
+
+def slope(u,limiter):
+    a = u[1:]-u[0:-1]
+    if limiter=='minmod':
+        S = minmod(a[0:-1],a[1:])
+    if limiter=='superbee':
+        S = superbee(a[0:-1],a[1:])
+    if limiter=='mc':
+        S = mc(2.*a[0:-1],.5*(u[2:]-u[:-2]),2.*a[1:])
+    return S
+
+
+class Road:
+
+    x_IN = None
+    x_OUT = None
+    n = None
+    dx = None
+
+    rho = None
+    q = None
+
+    rho_tmp = None
+    q_tmp = None
+
+    Umax = None
+    bc_IN = None
+    bc_OUT = None
+    bc_NEIGHBOUR = None
+
+    def rhoLR(self, o, limiter):
+        if o==1:
+            rhoL = self.rho[0:-1]
+            rhoR = self.rho[1:]
+        if o==2:
+            S = slope(self.rho,limiter)
+            rhoL = self.rho[1:-2] + 0.5*S[0:-1]
+            rhoR = self.rho[2:-1] - 0.5*S[1:]
+        return rhoL, rhoR
+
+    def applyBC(self, roads, o, t):
+        if self.bc_IN<0:
+            self.rho = BC_inflow(self.rho,o,t)
+        else:
+            if (t<6 or (t>8 and t<10) or (t>12 and t<15) or (t>16 and t<17) ):
+                for i in range(1,o+1):
+                    self.rho[o-i] = 0.
+            else:
+                for i in range(1,o+1):
+                    self.rho[o-i] = cp(roads[self.bc_IN].rho[-o-i])
+
+        if self.bc_OUT<0:
+            self.rho = BC_outflow(self.rho,o,t)
+        else:
+            if (t<6 or (t>8 and t<10) or (t>12 and t<15) or (t>16 and t<17) ):
+                for i in range(1,o+1):
+                    self.rho[-o+i-1] = 1.
+            else:
+                for i in range(1,o+1):
+                    self.rho[-o+i-1] = cp(roads[self.bc_OUT].rho[o+i-1])
+
+    def maxEig(self):
+        DF = Flux_drho(self.rho, self.Umax)
+        return max(np.abs(DF))
+
+    def maketmp(self):
+        self.rho_tmp = 1.*self.rho
+        self.q_tmp = 1.*self.q
+
+    def initializeRoad(self, n, geo_IN, geo_OUT, o, Umax=1.):
+        self.rho = np.zeros((n+2*o,1))
+        self.q = np.zeros((n+2*o,1))
+        self.Umax = Umax
+        self.geo_IN = geo_IN
+        self.geo_OUT = geo_OUT
+        self.dx = np.linalg.norm(geo_IN - geo_OUT)/(1.*n)
+
+def Flux(rho, Umax):
+    return rho*Umax*(1.-rho)
+
+def Flux_drho(rho, Umax):
+    return Umax*(1.-2.*rho)
+
 
 #def displayFun():
 #    glClear(GL_COLOR_BUFFER_BIT)
@@ -39,42 +121,29 @@ def initFun():
 
     glMatrixMode( GL_PROJECTION );
     glLoadIdentity();
-    glOrtho(-1,1,-1,1,-1,1)
+    glOrtho(-2,2,-2,2,-2,2)
 
     glMatrixMode( GL_MODELVIEW );
     glLoadIdentity();
 
 def main(n,o, limiter, solver):
-    Length = 2.
-    dx = Length/n
-    x = dx/2. + np.arange(n)*dx
-    rho_r1 = initializeRho(x,o)
-    rho_r2 = initializeRho(x,o)
-    rho = np.vstack([rho_r1,rho_r2])
 
-    #pl.ion()
-    ##pl.figure()
-    #line1, line2, = pl.plot(x,rho[0,o:-o],'g',Length+x,rho[1,o:-o],'r')
-    #line1.axes.set_ylim(-.1,1.1)
-    #line2.axes.set_ylim(-.1,1.1)
-    #pl.axes().set_aspect('equal')
-    #pl.draw()
+    roads = [Road() for i in range(2)]
+    for i in range(len(roads)):
+        roads[i].initializeRoad(n, np.array((-2.+i*2,.0)), np.array((-0.+i*2,0.)), o)
 
-    #pl.plot(x,rho[o:-o],label='t=0')
+    roads[0].bc_IN = -1
+    roads[0].bc_OUT = 1
+
+    roads[1].bc_IN = 0
+    roads[1].bc_OUT = -1
 
     T = 20
     Tintervals = np.linspace(0.,1.*T,num=T+1)
     for ti in range(Tintervals.shape[0]-1):
-        #print Tintervals[ti:ti+2]
-        rho = ConsLaw(x, rho, Tintervals[ti:ti+2], limiter, solver, o)#, line1, line2)
-        #pl.plot(x,rho[o:-o],label='t=%0.2f'%Tintervals[ti+1])
-    #pl.legend()
-    #pl.show()
+        roads = ConsLaw(roads, Tintervals[ti:ti+2], limiter, solver, o)
 
-def ConsLaw(x, rho, Tinterval, limiter, solver, o):#, line1, line2):
-
-    dx = x[1]-x[0]
-    n = x.shape[0]
+def ConsLaw(roads, Tinterval, limiter, solver, o):
 
     t = cp(Tinterval[0])
 
@@ -82,103 +151,70 @@ def ConsLaw(x, rho, Tinterval, limiter, solver, o):#, line1, line2):
     while t<Tinterval[1]:
         count += 1
 
-        DF = Flux_drho(rho[0,o:-o])
-        eig = max(np.abs(DF))
-        DF = Flux_drho(rho[1,o:-o])
-        eig = max(eig,max(np.abs(DF)))
+        eig = roads[0].maxEig()/roads[0].dx
+        for i in range(1,len(roads)):
+            eig = max(eig,roads[i].maxEig()/roads[i].dx)
 
         if (o==1):
             CFL = 0.99
         if (o==2):
             CFL = 0.49
 
-        dt=1.*CFL*dx/eig
+        dt = 1.*CFL/eig
 
         if t+dt>Tinterval[1]:
             dt=Tinterval[1]-t
         t=t+dt
 
         if o==2:
-            saverho = 1.*rho
+            for i in range(len(roads)):
+                roads[i].maketmp()
+
         for internalsteps in range(1,o+1):
-            rho[0,:] = BC_inflow(rho[0,:],o,t)
-            if (t<6 or (t>8 and t<10) or (t>12 and t<15) or (t>16 and t<17) ):
-                rho = BC_coupleRed(rho,o,0,1)
-            else:
-                rho = BC_coupleGreen(rho,o,0,1)
-            rho[1,:] = BC_outflow(rho[1,:],o,t)
-            ## construct uL and uR
-            if o==1:
-                rhoL = rho[:,0:-1]
-                rhoR = rho[:,1:]
-            if o==2:
-                # apply slope limiter
-                a = rho[:,1:]-rho[:,0:-1]
-                if limiter=='minmod':
-                    S = minmod(a[:,0:-1],a[:,1:])
-                if limiter=='superbee':
-                    S = superbee(a[:,0:-1],a[:,1:])
-                if limiter=='mc':
-                    S = mc(2.*a[:,0:-1],.5*(rho[:,2:]-rho[:,:-2]),2.*a[:,1:])
-                rhoL = rho[:,1:-2] + 0.5*S[:,0:-1]
-                rhoR = rho[:,2:-1] - 0.5*S[:,1:]
-            if solver=='LxF':
-                # Lax Friedrichs
-                Frho = 0.5*(Flux(rhoL) + Flux(rhoR)) - 0.5*dx/dt*(rhoR - rhoL)
-            if solver=='upwind':
-                s = 1.-(rhoL+rhoR)
-                Frho = np.zeros_like(rhoL)
-                Frho[np.nonzero((s>0.)&(rhoL<.5))] = Flux(rhoL[np.nonzero((s>0.)&(rhoL<.5))])
-                Frho[np.nonzero((s<0.)&(rhoR>.5))] = Flux(rhoR[np.nonzero((s<0.)&(rhoR>.5))])
-                #Frho[np.nonzero((s>0.))] = Flux(rhoL[np.nonzero((s>0.))])
-                #Frho[np.nonzero((s<0.))] = Flux(rhoR[np.nonzero((s<0.))])
-                Frho[np.nonzero((rhoL>.5)&(.5>rhoR))] = Flux(.5)
-            rho[:,o:-o] -= dt/dx*(Frho[:,1:] - Frho[:,0:-1])
+            ### apply boundary conditions for all roads
+            for i in range(len(roads)):
+                roads[i].applyBC(roads, o, t)
+            ### advance FV scheme
+            for i in range(len(roads)):
+                rhoL, rhoR = roads[i].rhoLR(o, limiter)
+                if solver=='LxF':
+                    # Lax Friedrichs
+                    Frho = 0.5*(Flux(rhoL, roads[i].Umax) + Flux(rhoR, roads[i].Umax)) - 0.5*roads[i].dx/dt*(rhoR - rhoL)
+                if solver=='upwind':
+                    s = 1.-(rhoL+rhoR)
+                    Frho = np.zeros_like(rhoL)
+                    Frho[np.nonzero((s>0.)&(rhoL<.5))] = Flux(rhoL[np.nonzero((s>0.)&(rhoL<.5))], roads[i].Umax)
+                    Frho[np.nonzero((s<0.)&(rhoR>.5))] = Flux(rhoR[np.nonzero((s<0.)&(rhoR>.5))], roads[i].Umax)
+                    #Frho[np.nonzero((s>0.))] = Flux(rhoL[np.nonzero((s>0.))])
+                    #Frho[np.nonzero((s<0.))] = Flux(rhoR[np.nonzero((s<0.))])
+                    Frho[np.nonzero((rhoL>.5)&(.5>rhoR))] = Flux(.5, roads[i].Umax)
+                roads[i].rho[o:-o] -= dt/roads[i].dx*(Frho[1:] - Frho[0:-1])
         # end loop for order
         if o==2:
-            rho = 0.5*(rho + saverho)
+            for i in range(len(roads)):
+                roads[i].rho = 0.5*(roads[i].rho + roads[i].rho_tmp)
 
         if (count%10==0):
-            #line1.set_ydata(rho[0,o:-o])
-            #line2.set_ydata(rho[1,o:-o])
-            #pl.draw()
-
-
             glEnable( GL_TEXTURE_1D );
             glBindTexture( GL_TEXTURE_1D, texture );
-            for road in np.array((0,1)):
-                glTexImage1D(GL_TEXTURE_1D, 0, GL_RED, rho[road,o:-o].shape[0], 0, GL_RED, GL_FLOAT, rho[road,o:-o]);
+            for i in range(len(roads)):
+                glTexImage1D(GL_TEXTURE_1D, 0, GL_RED, roads[i].rho[o:-o].shape[0], 0, GL_RED, GL_FLOAT, roads[i].rho[o:-o]);
                 glColor3ub( 255, 255, 255 );
 
                 glLineWidth( 10 );
                 glEnable(GL_LINE_SMOOTH);
                 glBegin( GL_LINES );
                 glTexCoord1i( 0 );
-                glVertex2f( -0.5+road/2., 0. );
+                glVertex2f( roads[i].geo_IN[0], roads[i].geo_IN[1]);
                 glTexCoord1i( 1 );
-                glVertex2f(  0.0+road/2., 0. );
+                glVertex2f( roads[i].geo_OUT[0], roads[i].geo_OUT[1]);
                 glEnd();
 
             glFlush()
     # end while
-    return rho
+    return roads
 
 
-def Flux(rho):
-    umax = 1.
-    return rho*umax*(1.-rho)
-
-def Flux_drho(rho):
-    umax = 1.
-    return umax*(1.-2*rho)
-
-def initializeRho(x,o):
-    # initialize function
-    rho = 0.*x
-    # add ghost cells
-    for i in range(1,o+1):
-        rho = np.hstack([0.,rho,0.])
-    return rho
 
 def BC_inflow(rho,o,t):
     rho[0] = max(0,np.sin(6*t))
@@ -213,7 +249,7 @@ def superbee(x,y):
     return maxmod(minmod(x,2*y),minmod(2*x,y))
 
 def mc(x,y,z):
-    # elements with not equal signs
+    # elements with unequal signs
     dif = (((x<0.)&(y<0.)&(z<0.))!=True)&(((x>0.)&(y>0.)&(z>0.))!=True)
     x[np.nonzero(dif)]=0.0
     y[np.nonzero(dif)]=0.0
